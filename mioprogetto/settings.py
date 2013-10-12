955c85a01a06dfcbe05c79da0a88fe5bdd6b38ff
monicarelifeit <monica.bramuzzi@relifeit.com>
2013-10-12 12:57:49 +0200

    Database riscritto in settigs.py

--- mioprogetto/settings.py
+++ mioprogetto/settings.py
@@ -178,7 +178,21 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
     )
 }
-import settings_heroku.py
+
+from mioprogetto.settings import *
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+
+
 # Static asset configuration
 import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
